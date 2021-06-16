fa76a883fec539e4a9139e004cb05e0400cf2a34
Osman <osmanozevin@gmail.com>
2021-06-16 15:43:06 +0100

    replaced public key with amazonRootCA1 in choosesite

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ class AddUserOut(BaseModel):
     
 class ChooseSiteOut(BaseModel):
     certificatePem: str = Field(..., example="-----BEGIN CERTIFICATE-----\nMIIDWjCCAkKgAwIBAgIVAMQz1aj7fE0nUqxhC.....")
-    publicKey: str = Field(..., example="-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBC.....")
+    amazonRootCA1: str = Field(..., example="-----BEGIN CERTIFICATE-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBC.....")
     privateKey: str = Field(..., example="-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAupYWWqr+O0XLIEJlA/n8g.....")
     
     
@@ -315,6 +315,30 @@ def loginUser(email: str, password: str):
         return None
 
 def createNewThingAndKeys(name: str):
+    
+    amazonRootCA1 = '''
+-----BEGIN CERTIFICATE-----
+MIIDQTCCAimgAwIBAgITB`myfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
+ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6
+b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL
+MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv
+b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj
+ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM
+9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw
+IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6
+VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L
+93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm
+jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC
+AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA
+A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI
+U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs
+N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv
+o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU
+5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy
+rqXRfboQnoZsG4q5WTP468SQvvG5
+-----END CERTIFICATE-----
+'''
+    
     try:
         principals = client.list_thing_principals(thingName=name)
         print(principals)
@@ -336,7 +360,7 @@ def createNewThingAndKeys(name: str):
     
     return {
         "certificatePem": createKeysResponse["certificatePem"],
-        "publicKey": createKeysResponse["keyPair"]["PublicKey"],
+        "amazonRootCA1": amazonRootCA1,
         "privateKey": createKeysResponse["keyPair"]["PrivateKey"],
     }
 
