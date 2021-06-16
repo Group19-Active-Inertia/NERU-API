b916c5c9425be829ca212fec39426cb31d2e20d9
Osman <osmanozevin@gmail.com>
2021-06-16 16:44:45 +0100

    fix for createNewThingAndKeys

--- main.py
+++ main.py
@@ -316,8 +316,7 @@ def loginUser(email: str, password: str):
 
 def createNewThingAndKeys(name: str):
     
-    amazonRootCA1 = '''
------BEGIN CERTIFICATE-----
+    amazonRootCA1 = '''-----BEGIN CERTIFICATE-----
 MIIDQTCCAimgAwIBAgITB`myfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
 ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6
 b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL
